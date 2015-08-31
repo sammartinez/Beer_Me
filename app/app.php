37e0e20ebb9a9311ec30a0ffc14397035cb85248
Kelli Margaret & Jordan Johansen <student@epicodus.com>
2015-08-31 15:22:36 -0700

    Create route for homepage.

--- app/app.php
+++ app/app.php
@@ -1,5 +1,31 @@
 <?php
 
-    
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Bar.php";
+    require_once __DIR__."/../src/Item.php";
+    require_once __DIR__."/../src/Patron.php";
 
- ?>
+    $app = New Silex\Application();
+
+    $server = 'mysql:host=localhost;dbname=beer';
+    $username = 'root';
+    $password = 'root';
+
+    $DB = new PDO($server, $username, $password);
+
+    //Twig Path
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path'=>__DIR__."/../views"
+));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    //Get Calls
+    $app->get("/", function() use($app) {
+        return $app['twig']->render("index.html.twig");
+    });
+
+    return $app;
+
+?>
